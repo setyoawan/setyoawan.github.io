461ff904e7a979a9a0b3702c53448e6f72302d8d
setyoawan <setyoawan8@gmail.com>
2020-08-10 20:03:39 +0700

    initial commit

---
+++ regist.js
@@ -0,0 +1,15 @@
+// register service worker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/service-worker.js")
+            .then(function () {
+                console.log("success");
+            })
+            .catch(function () {
+                console.log("failed");
+            });
+    });
+} else {
+    console.log("not support");
+}
